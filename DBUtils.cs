a8a2376b793bbfb3254c0f7739e1f7d5d94dee8f
комп <@KATYHOME>
2024-06-02 22:58:51 +0300

    Добавьте файлы проекта.

---
+++ DBUtils.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+namespace Лр3_1
+{
+    class DBUtils
+    {
+        public static MySqlConnection GetDBConnection()
+        {
+            //connection string
+            string host = "localhost";
+            int port = 3306;
+            string database = "payment_for_the_tariff_";
+            string username = "andrey";
+            string password = "1111";
+            return DBMySQLUtils.GetDBConnection(host, port, database, username, password);
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Data.Common;
+using System.Linq.Expressions;
+using Microsoft.VisualBasic;
+using MySql.Data.MySqlClient;
+
+namespace Лр3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//Получить объект Connection подключенный к DB.
+            Console.WriteLine("Getting Connection...");
+            MySqlConnection conn = DBUtils.GetDBConnection();
+            try
+            {
+                Console.WriteLine("Opening Connection...");
+                conn.Open();
+                Console.WriteLine("Connection successful!");
+                QueryEmployee(conn);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
+            finally
+            {
+                //Закрыть соединение
+                conn.Close();
+                //Уничтожить объект, освободить ресурс
+                conn.Dispose();
+            }
+            Console.Read();
+        }
+        private static void QueryEmployee(MySqlConnection conn)
+        {
+            string id, lastname, name, middlename, phone_number, tariff_id;
+            string sql = "Select abonent_id, lastname, name, middlename, phone_number, tariff_id from abonent";
+
+            //создать объект Command
+            MySqlCommand cmd = new MySqlCommand();
+
+            //сочетать Command c Connection
+            cmd.Connection = conn;
+            cmd.CommandText = sql;
+            using (DbDataReader reader = cmd.ExecuteReader())
+            {
+                if (reader.HasRows)
+                {                                     
+                   while (reader.Read())
+                   {
+                        id = reader["abonent_id"].ToString();
+                        lastname = reader["lastname"].ToString();
+                        name = reader["name"].ToString();
+                        middlename = reader["middlename"].ToString();
+                        phone_number = reader["phone_number"].ToString();
+                        tariff_id = reader["tariff_id"].ToString();
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Код:" + id +" Прiзвище:"+ lastname + " Iм'я:"+ name + " По-батьковi:" + 
+                            middlename + " Номер телефону:" + phone_number + " Тарифний план:" + tariff_id);
+                   }
+                    
+                }
+            }
+        }
+
+    }
+}
